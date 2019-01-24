6ebe251fb858df28c89783ed43ee445b8700b65a
Robert Glover <robert_f_g@hotmail.com>
2019-01-24 11:48:35 -0500

    Plant revised by mentor.

---
+++ InterfacesAndStatics/Plant - Revised.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesAndStatics
+{
+    class PlantRevised : IPlant
+    {
+        public int Test { get; private set; }
+
+        public void GrowthRate()
+        {
+            int test = this.Test;
+            string anotherTest = GetString();
+
+            throw new NotImplementedException();
+        }
+
+        private string GetString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
